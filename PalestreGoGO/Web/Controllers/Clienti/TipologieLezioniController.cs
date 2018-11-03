593dbedbfc92765efc67c4b4d740b404d20a3968
gianluca.t <gianluca_tofi79@hotmail.it>
2018-11-03 20:33:35 +0100

    Refactoring Tipologie Lezioni

---
+++ PalestreGoGO/Web/Controllers/Clienti/TipologieLezioniController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Configuration;
+using Web.Services;
+using Web.Utils;
+
+namespace Web.Controllers.Clienti
+{
+    [Authorize(AuthenticationSchemes = OpenIdConnectDefaults.AuthenticationScheme)]
+    [Route("/{cliente}/lezioni")]
+    public class TipologieLezioniController: Controller
+    {
+        private readonly ILogger<TipologieLezioniController> _logger;
+        private readonly AppConfig _appConfig;
+        private readonly WebAPIClient _apiClient;
+        private readonly ClienteResolverServices _clientiResolver;
+
+        public TipologieLezioniController(ILogger<TipologieLezioniController> logger,
+                                 IOptions<AppConfig> apiOptions,
+                                 WebAPIClient apiClient,
+                                 ClienteResolverServices clientiResolver)
+        {
+            _logger = logger;
+            _appConfig = apiOptions.Value;
+            _apiClient = apiClient;
+            _clientiResolver = clientiResolver;
+        }
+
+
+        #region Gestione Tipologie Lezioni
+        [HttpGet]
+        public async Task<IActionResult> ListaLezioni([FromRoute(Name = "cliente")]string urlRoute)
+        {
+            int idCliente = await _clientiResolver.GetIdClienteFromRouteAsync(urlRoute);
+            //Verifichiamo che solo gli Admin possano accedere alla pagina di Edit Profilo
+            if (!User.GetUserTypeForCliente(idCliente).IsAtLeastAdmin())
+            {
+                return Forbid();
+            }
+            ViewData["IdCliente"] = idCliente;
+            var lezioni = await _apiClient.GetTipologieLezioniClienteAsync(idCliente);
+            return View("ListaLezioni", lezioni.ToList());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> LezioneEdit([FromRoute(Name = "cliente")]string urlRoute, [FromRoute(Name = "id")]int idLezione)
+        {
+            int idCliente = await _clientiResolver.GetIdClienteFromRouteAsync(urlRoute);
+            //Verifichiamo che solo gli Admin possano accedere alla pagina di Edit Sale
+            if (!User.GetUserTypeForCliente(idCliente).IsAtLeastAdmin())
+            {
+                return Forbid();
+            }
+            ViewData["IdCliente"] = idCliente;
+
+            Models.TipologieLezioniViewModel tipoLezione = null;
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            if (idLezione > 0)
+            {
+                tipoLezione = await _apiClient.GetOneTipologiaLezione(idCliente, idLezione, accessToken);
+            }
+            if (tipoLezione == null)
+            {
+                return NotFound();
+            }
+            return View("LezioneEdit", tipoLezione);
+        }
+
+        [HttpGet("new")]
+        public async Task<IActionResult> LezioneAdd([FromRoute(Name = "cliente")]string urlRoute)
+        {
+            //var cliente = await _apiClient.GetClienteAsync(urlRoute);
+            int idCliente = await _clientiResolver.GetIdClienteFromRouteAsync(urlRoute);
+            //Verifichiamo che solo gli Admin possano accedere alla pagina di Edit Sale
+            if (!User.GetUserTypeForCliente(idCliente).IsAtLeastAdmin())
+            {
+                return Forbid();
+            }
+            ViewData["IdCliente"] = idCliente;
+            Models.TipologieLezioniViewModel tipoLezione = new Models.TipologieLezioniViewModel();
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            return View("LezioneEdit", tipoLezione);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> LezioneSave([FromRoute(Name = "cliente")]string urlRoute, [FromForm] Models.TipologieLezioniViewModel tipoLezione)
+        {
+            //var cliente = await _apiClient.GetClienteAsync(urlRoute);
+            int idCliente = await _clientiResolver.GetIdClienteFromRouteAsync(urlRoute);
+            ViewData["IdCliente"] = idCliente;
+            //Verifichiamo che solo gli Admin possano accedere alla pagina di Edit Sale
+            if (!User.GetUserTypeForCliente(idCliente).IsAtLeastAdmin())
+            {
+                return Forbid();
+            }
+            if (!ModelState.IsValid)
+            {
+                return View("LezioneEdit", tipoLezione);
+            }
+            if (tipoLezione.Id.HasValue && (tipoLezione.Id.Value <= 0)) { tipoLezione.Id = null; }
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            await _apiClient.SaveTipologiaLezioneAsync(idCliente, tipoLezione, accessToken);
+            return RedirectToAction("ListaLezioni");
+        }
+
+        [HttpGet("delete/{id}")]
+        public async Task<IActionResult> LezioneDelete([FromRoute(Name = "cliente")]string urlRoute, [FromRoute(Name ="id")] int idLezione)
+        {
+            //var cliente = await _apiClient.GetClienteAsync(urlRoute);
+            int idCliente = await _clientiResolver.GetIdClienteFromRouteAsync(urlRoute);
+            //Verifichiamo che solo gli Admin possano accedere alla pagina di Edit Sale
+            if (!User.GetUserTypeForCliente(idCliente).IsAtLeastAdmin())
+            {
+                return Forbid();
+            }
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            await _apiClient.DeleteOneTipologiaLezioneAsync(idCliente, idLezione, accessToken);
+            return RedirectToAction("ListaLezioni");
+        }
+
+        #endregion
+    }
+}
--- PalestreGoGO/Web/Models/TipologieLezioniViewModel.cs
+++ PalestreGoGO/Web/Models/TipologieLezioniViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,9 +9,32 @@ namespace Web.Models
 {
     public class TipologieLezioniViewModel
     {
+        public List<SelectListItem> IntervalliTempo { get; } = new List<SelectListItem>()
+        {
+                 new SelectListItem("00:15", "15"),
+                 new SelectListItem("00:30", "30"),
+                 new SelectListItem("00:45", "45"),
+                 new SelectListItem("01:00", "60"),
+                 new SelectListItem("01:30", "90"),
+                 new SelectListItem("02:00", "120"),
+                 new SelectListItem("02:30", "150"),
+                 new SelectListItem("03:00", "180"),
+                 new SelectListItem("04:00", "240"),
+                 new SelectListItem("05:00", "300"),
+                 new SelectListItem("06:00", "360"),
+                 new SelectListItem("12:00", "720"),
+                 new SelectListItem("24:00", "1440"),
+                 new SelectListItem("48:00", "2880")
+        };
+
         public int? Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Nome { get; set; }
+
+        [StringLength(500)]
         public string Descrizione { get; set; }
+
         public int Durata { get; set; }
         public int? MaxPartecipanti { get; set; }
         public short? LimiteCancellazioneMinuti { get; set; }
